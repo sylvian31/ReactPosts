6eea225582825e4cf7eee360924e61635855958a
sylvian <sylvian31@gmail.com>
2019-04-19 16:01:43 -0400

    squelette

---
+++ src/containers/post-list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostList extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Liste des posts</h1>
+            </div>
+        )
+    }
+}
+
+export default PostList
